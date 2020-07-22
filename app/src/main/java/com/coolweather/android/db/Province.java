b4cf9b89e5758640b430d0d4c218cdc4e87ffe70
Tony <tony@gmail.com>
2020-07-22 16:43:55 +0800

    加入添加数据库和各项表的配置

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,33 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String ProvinceName;
+    private int ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return ProvinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        ProvinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
